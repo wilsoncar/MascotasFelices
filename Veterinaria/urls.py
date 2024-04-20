8f95f3f223d94be04f46383490af8215086eccef
wilson aguirre <wilsoncardona73@gmail.com>
2024-04-20 01:05:25 -0600

    Modificaciones a vistas,urls y archivo docker-compose.yml

--- Veterinaria/urls.py
+++ Veterinaria/urls.py
@@ -4,17 +4,17 @@ from .views import DuenosViewSet, MascotasViewSet, CitasViewSet, Desparasitacion
 urlpatterns = [
     #urls duenos
     path('duenos/', DuenosViewSet.as_view({'get': 'list', 'post': 'create'}), name='duenos'),
-    path('duenos/<int:pk>', DuenosViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='dueno_detalles'),
+    path('duenos/<int:pk>', DuenosViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update'}), name='dueno_detalles'),
 
     #urls mascotas
     path('mascotas/', MascotasViewSet.as_view({'get': 'list', 'post': 'create'}), name='mascotas'),
-    path('mascotas/<int:pk>', MascotasViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='mascota_detalles'),
+    path('mascotas/<int:pk>', MascotasViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update'}), name='mascota_detalles'),
 
     #urls citas
     path('citas/', CitasViewSet.as_view({'get': 'list', 'post': 'create'}), name='citas'),
-    path('citas/<int:pk>', CitasViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='cita_detalles'),
+    path('citas/<int:pk>', CitasViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update'}), name='cita_detalles'),
 
     #urls desparasitaciones
     path('desparasitaciones/', DesparasitacionesViewSet.as_view({'get': 'list', 'post': 'create'}), name='desparasitaciones'),
-    path('desparasitaciones/<int:pk>', DesparasitacionesViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='desparasitaciones_detalles'),
+    path('desparasitaciones/<int:pk>', DesparasitacionesViewSet.as_view({'get': 'retrieve','put': 'update', 'patch': 'partial_update'}), name='desparasitaciones_detalles'),
 ]
--- Veterinaria/views.py
+++ Veterinaria/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Duenos, Mascotas, Citas, Desparasitaciones
 from .serializers import DuenosSerializer, MascotasSerializer, CitasSerializer, DesparasitacionesSerializer
 from rest_framework.response import Response
@@ -10,20 +10,87 @@ from rest_framework.response import Response
 class DuenosViewSet(viewsets.ModelViewSet):
     queryset = Duenos.objects.all()
     serializer_class = DuenosSerializer
-    permission_classes = []
+
+    def list(self, request, *args, **kwargs):
+        queryset = Duenos.objects.all().filter(status="Active")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        response_data = {
+            "message": "Dueño creado",
+            "Dueño": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
     
 
 class MascotasViewSet(viewsets.ModelViewSet):
     queryset = Mascotas.objects.all()
     serializer_class = MascotasSerializer
-    permission_classes = []
+
+    def list(self, request, *args, **kwargs):
+        queryset = Mascotas.objects.all().filter(status="Active")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        response_data = {
+            "message": "Mascota creada",
+            "Mascota": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+    
 
 class CitasViewSet(viewsets.ModelViewSet):
     queryset = Citas.objects.all()
     serializer_class = CitasSerializer
-    permission_classes = []
+
+    def list(self, request, *args, **kwargs):
+        queryset = Citas.objects.all().filter(status="Done")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        response_data = {
+            "message": "Cita creada",
+            "Cita": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+    
 
 class DesparasitacionesViewSet(viewsets.ModelViewSet):
     queryset = Desparasitaciones.objects.all()
     serializer_class = DesparasitacionesSerializer
-    permission_classes = []
+
+    def list(self, request, *args, **kwargs):
+        queryset = Desparasitaciones.objects.all().filter(status="Completado")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        response_data = {
+            "message": "Desparasitación creada",
+            "Desparasitación": serializer.data
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+    
